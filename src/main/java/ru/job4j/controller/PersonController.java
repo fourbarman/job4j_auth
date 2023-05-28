260cd2b3b297efb17452ffd06ad987547406ab43
fourbarman <maks.java@yandex.ru>
2023-05-28 15:51:16 +0400

    findAll method returns ResponseEntity.

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -27,8 +27,8 @@ public class PersonController {
     private final BCryptPasswordEncoder encoder;
 
     @GetMapping("/")
-    public List<Person> findAll() {
-        return this.persons.findAll();
+    public ResponseEntity<List<Person>> findAll() {
+        return new ResponseEntity<>(this.persons.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
